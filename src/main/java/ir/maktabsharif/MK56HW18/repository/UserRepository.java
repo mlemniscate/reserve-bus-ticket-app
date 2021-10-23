983196024cdf187d71adf75685fc59ad7fb71c1f
Milad Abdi <miladabdi1374@gmail.com>
2021-10-23 13:50:48 +0330

    Add repositories

---
+++ src/main/java/ir/maktabsharif/MK56HW18/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ir.maktabsharif.MK56HW18.repository;
+
+import ir.maktabsharif.MK56HW18.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
