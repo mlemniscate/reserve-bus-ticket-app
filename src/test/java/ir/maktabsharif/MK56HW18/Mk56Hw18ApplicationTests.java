9ada75c277bd6520b180827d76c1973e372ca388
Milad Abdi <miladabdi1374@gmail.com>
2021-10-23 13:10:34 +0330

    Add and configure all necessaries for this project.

---
+++ src/test/java/ir/maktabsharif/MK56HW18/Mk56Hw18ApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif.MK56HW18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mk56Hw18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
